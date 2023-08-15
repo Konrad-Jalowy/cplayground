217cdd09979bdf2f80c7065acd803feddc795cdd
Konrad-Jalowy <sitesystem@wp.pl>
2023-08-15 11:31:15 +0200

    exercise

---
+++ rectarea.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	double height, width;
+	double area;
+	printf("Enter height: ");
+	scanf("%lf", &height);
+	printf("Enter width: ");
+	scanf("%lf", &width);
+	area = height * width;
+	printf("The area of this rectangle = %lf\n", area);
+	return 0;
+}
