8696b370d91b067718305b88fce1032b2eca7096
Konrad-Jalowy <sitesystem@wp.pl>
2023-08-15 11:19:02 +0200

    simplesimple

---
+++ helloname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+
+
+    char arr[11];
+    printf("Enter any 10 character name : \n");
+    scanf("%10s", arr);
+    printf("Hello %s", arr);
+
+
+         return 0;
+}
+
